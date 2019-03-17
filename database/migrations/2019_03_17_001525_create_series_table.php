1635cd1fdd73131fadd2202e265d7313b27fce20
phaelteixeira <45774900+phaelteixeira@users.noreply.github.com>
2019-03-16 21:39:05 -0300

    Foram feitas as configurações do banco de dados, criado o projeto laravel, migrations, seeder e model.

---
+++ database/migrations/2019_03_17_001525_create_series_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSeriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('series', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('sinopse');
+            $table->integer('genero_id')->unsigned();
+            $table->foreign('genero_id')->references('id')->on('generos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('series');
+    }
+}
---
+++ database/seeds/GenerosTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class GenerosTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('generos')->insert([
+            'nome'=>'Comédia'    
+        ]);
+        DB::table('generos')->insert([
+            'nome'=>'Drama'    
+        ]);
+        DB::table('generos')->insert([
+            'nome'=>'Ficção Cientifica'
+        ]);
+        DB::table('generos')->insert([
+            'nome'=>'Suspense'    
+        ]);
+    }
+}
